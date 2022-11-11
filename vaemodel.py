9db3b6bd21844ef2ff62788916afcaba19a54854
Altan Senel <altan@abios.se>
2022-11-11 21:10:25 +0100

    skeleton model for mae

---
+++ vaemodel.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class MaskedAutoencoderViT(nn.Module):
+    def __init__(self):
+        pass
+
+    def _init_weights(self):
+        pass
+
+    def patchify(self, imgs):
+        pass
+
+    def mask(self):
+        pass
+
+    def encoder_forward(self, x):
+        pass
+
+    def decoder_forward(self, x):
+        pass
+
+    def forward(self, imgs):
+        pass
+
+    def loss(self):
+        pass
