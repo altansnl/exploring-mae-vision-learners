26582caf429ab9abe3b1ffd6e411084e36dfac67
Altan Senel <altan@abios.se>
2022-11-11 21:25:50 +0100

    add comments

--- vaemodel.py
+++ vaemodel.py
@@ -7,21 +7,36 @@ class MaskedAutoencoderViT(nn.Module):
 
     def _init_weights(self):
         pass
-
+    
+    # divide an image into regular non-overlapping patches
     def patchify(self, imgs):
         pass
 
+    # sample a subset of patches and mask (remove) the remaining ones
+    # random without replacement
     def mask(self):
         pass
-
+    
+    # apply VIT on visible/unmasked patches
     def encoder_forward(self, x):
-        pass
+        # encode patches with linear projection
+        # with added position embeddings
 
+        # apply set of transformer blocks
+        pass
+    
+    # input: full set of tokens consisting of
+    # (i) encoded visible patches, (ii) mask tokens
     def decoder_forward(self, x):
-        pass
+        # add positional embeddings to tokens
 
+        # apply series of transformer blocks
+        pass
+    
     def forward(self, imgs):
         pass
 
+    # mean squared error in the pixel space
+    # calculated only on masked patches
     def loss(self):
         pass
