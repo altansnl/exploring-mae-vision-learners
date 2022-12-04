e9cf9b6440d5a235c00f8bbed727100d67af2d75
AGarciaCast <a.gcastellanos@alumnos.upm.es>
2022-12-04 11:44:09 +0100

    testing transfer learning

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from maemodel import *
 import numpy as np
+from functools import partial
+from collections import OrderedDict
 
 def set_seed(seed):
     np.random.seed(seed)
@@ -10,19 +12,19 @@ def set_seed(seed):
         
 set_seed(42)
 
+model = VisionTransformer(
+            img_size=64,
+            patch_size=16,
+            in_chans=3,
+            num_classes=10,
+            embed_dim=768,
+            depth=12,
+            num_heads=12,
+            mlp_ratio=4.,
+        )
 
-imgs = torch.rand((1, 3, 64, 64))
-set_seed(42)
-model_facebook = MaskedAutoencoderViT(img_size=(64,64),
-    patch_size=16, embed_dim=768, depth=12, num_heads=12,
-    decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
-    mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6))
+#print(model)
 
-set_seed(42)
-loss_1, pred_1, mask_1 = model_facebook.forward(imgs, mask_ratio=0.75)
-print(loss_1)
-
-set_seed(42)
 our_model = MAEPretainViT(
         img_dim=64,
         num_channels=3,
@@ -40,9 +42,35 @@ our_model = MAEPretainViT(
         norm2=partial(nn.LayerNorm, eps=1e-6)
     )
 
-set_seed(42)
-pred_2, mask_2 = our_model.forward(imgs)
-loss_2 = our_model.loss(imgs, pred_2, mask_2, norm_tar=False)
-print(loss_2)
-print(mask_1)
-print(torch.abs(loss_1-loss_2))
+
+model_translation = {
+    "input_layer": "patch_embed",
+    "backbone": "blocks",
+    "pos_embedding": "pos_embed",
+    "norm": "norm" # depending on the timm implementation has a different name
+}
+
+
+checkpoint_model = our_model.state_dict()
+all_keys = list(checkpoint_model.keys())
+new_dict = OrderedDict()
+
+for key in all_keys:
+    if key.startswith('encoder.'):
+        key_new = key[8:]
+        for mae_name, vit_name in model_translation.items():
+            key_new = key_new.replace(mae_name, vit_name)
+            
+        new_dict[key_new] = checkpoint_model[key]
+        
+    elif key.startswith('decoder.'):
+        print(f"Removing key {key} from pretrained checkpoint")
+        del checkpoint_model[key]
+    else:
+        new_dict[key] = checkpoint_model[key]
+        
+checkpoint_model = new_dict
+print("="*100)
+
+msg = model.load_state_dict(checkpoint_model, strict=False)
+print(msg)
